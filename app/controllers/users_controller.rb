0b879156258097038856d7ce78604b18334dfb45
mrogach2350 <m.rogach@gmail.com>
2016-10-05 11:05:24 -0700

    adds create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
     @users_count = User.count
   end
   def new
-    
+    @user = User.new
+  end
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
