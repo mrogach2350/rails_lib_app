326d133c502a44cdf9f955f97d7a1b20e9a4c11f
mrogach2350 <m.rogach@gmail.com>
2016-10-05 21:55:39 -0700

    refactors create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.find_by_id(params[:id])
   end
 
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
